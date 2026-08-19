4f171d264714dae60e06abe035f25038dd8633b7
Joseph <josephshaughton@gmail.com>
2026-08-19 11:51:36 +0100

    made enemys attack

--- enemy.py
+++ enemy.py
@@ -1,6 +1,7 @@
 from sprite import sprite
 import math
 import pygame
+import random
 import time
 
 class enemy(sprite):
@@ -11,7 +12,7 @@ class enemy(sprite):
       self.big       = big
       self.lastmove  = [0,0]
       self.Kback     = 0
-      self.attack    = 0
+      self.attackT   = 300
       self.Acooldown = 0
       self.wait      = 0
       self.sign      = False  
@@ -23,6 +24,7 @@ class enemy(sprite):
  
    def update(self,player,move,enemy_list,keys,place,screen,weaponList):
       self.image_index = 0
+      self.attackT += 1
       if self.ha <= 0:
          enemy_list.remove(self)
          return
@@ -34,11 +36,11 @@ class enemy(sprite):
       if weaponList[player.tool[player.wep]].attacking == False:
          self.iframes = False
 
-      if self.isHitXY(self.x+self.velocityX,self.y+self.velocityY,self.w,self.h,player) and self.attacking and not player.iFrames:
+      if self.isHitXY(self.x+self.velocityX,self.y+self.velocityY,self.w+self.velocityX,self.h+self.velocityY,player) and self.attacking and not player.iFrames:
          player.health     -= 1
          player.hit         = True
          player.image_index = 1
-         self.attack        = 0
+         self.attackT       = 300
          self.attacking     = False
 
       else:
@@ -56,22 +58,12 @@ class enemy(sprite):
          self.idle(player,screen,place)
       
 
-      if (abs(self.x - player.x) <= 100 or abs(self.y - player.y) <= 100) and self.Acooldown < 0:
-         self.attack = 10
-         self.image_index = 1
-         if not self.sign:
-            self.wait = 70
-            self.sign = True
-
-      if self.attack > 0 and self.wait < 0:
-         self.attacking   = True
-         self.velocityX  += self.lastmove[0]*3
-         self.velocityY  += self.lastmove[1]*3
-         self.attack     -= 1
-         self.Acooldown   = 200
-         self.image_index = 0
-         self.sign = False
-      else:
+      if self.LOS(8,player,place) and self.attackT > 1000 and random.randint(0,20) == 1:
+         self.attackT = 0
+         self.attack(player)
+      if self.attackT < 100:
+         self.attack(player)
+      if self.attackT > 300:
          self.attacking = False
 
       if (self.isHit(weaponList[player.tool[player.wep]]) and weaponList[player.tool[player.wep]].attacking) and not self.iframes:
@@ -113,14 +105,29 @@ class enemy(sprite):
          for tile in self.circleTiles:
             if tile in player.circleTiles:
                if player.LOS(8,tile,place) and self.LOS(8,tile,place):
-#                  x,y = screen.convertWTS(tile.x,tile.y)
- #                 pygame.draw.rect(screen.screen,(250,250,250),pygame.Rect(x,y,58,58),2)
+ #                 x,y = screen.convertWTS(tile.x,tile.y)
+#                  pygame.draw.rect(screen.screen,(250,250,250),pygame.Rect(x,y,58,58),2)
                   distanceX       = self.x - tile.x
                   distanceY       = self.y - tile.y
-                  totalDistance   = distanceX + distanceY
-                  self.velocityX -= (0.1/totalDistance)*distanceX
-                  self.velocityY -= (0.1/totalDistance)*distanceY
+                  totalDistance   = abs(distanceX) + abs(distanceY)
+                  self.velocityX -= (0.2/totalDistance)*distanceX
+                  self.velocityY -= (0.2/totalDistance)*distanceY
                   return
       self.chasing = False
       return
 
+   def attack(self,player):
+      if self.attackT < 70:
+         self.image_index = 1
+         if self.iframes:
+            self.attackT     = 300
+            self.attacking   = False
+            self.image_index = 0
+      elif self.attackT < 100:
+          self.attacking = True
+          self.image_index = 0
+          distanceX       = self.x - player.x
+          distanceY       = self.y - player.y
+          totalDistance   = abs(distanceX) + abs(distanceY)
+          self.velocityX -= (10/totalDistance)*distanceX
+          self.velocityY -= (10/totalDistance)*distanceY
